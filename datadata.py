5dd2f08fe9a513df9b42922258cafa26f936cabf
Jr <75345328+thejrcodingjourney@users.noreply.github.com>
2020-12-04 19:21:11 -0600

    Create datadata.py

---
+++ datadata.py
@@ -0,0 +1,15 @@
+#DATOS PARA ESCANEO Y MANDAR MENSAJE
+
+my_mac_address = ['dirección mac de tu dispositivo']
+
+local_ip = '192.168.1.0-254' #normalmente esta no cambia pero aun asi checala en tu red
+
+account = ["tu correo electronico de donde enviaras los mensajes"]
+
+pswd = ["tu contraseña del correo"]
+
+f_account = ["correo a donde enviarás las notificaciones"]
+
+server = ['smtp.gmail.com'] #cambia segun tu correo electronico si es hotmail o gmail
+
+port = 587 #normalemnte esto no cambia, pero consultalo con tu proveedor de servicios de correo
